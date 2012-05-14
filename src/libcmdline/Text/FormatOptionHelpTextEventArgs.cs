995c925012d673471a2a0a96bae27d02e85f7972
Charlie Cordova <charlie@thecordovas.net>
2012-05-14 09:02:03 -0400

    reorganizing to make it easier to see what's going on

---
+++ src/libcmdline/Text/FormatOptionHelpTextEventArgs.cs
@@ -0,0 +1,32 @@
+namespace CommandLine.Text
+{
+    using System;
+
+    /// <summary>
+    /// Provides data for the FormatOptionHelpText event.
+    /// </summary>
+    public class FormatOptionHelpTextEventArgs : EventArgs
+    {
+        private readonly BaseOptionAttribute _option;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLine.Text.FormatOptionHelpTextEventArgs"/> class.
+        /// </summary>
+        /// <param name="option">Option to format.</param>
+        public FormatOptionHelpTextEventArgs(BaseOptionAttribute option)
+        {
+            _option = option;
+        }
+
+        /// <summary>
+        /// Gets the option to format.
+        /// </summary>
+        public BaseOptionAttribute Option
+        {
+            get
+            {
+                return _option;
+            }
+        }
+    }
+}
